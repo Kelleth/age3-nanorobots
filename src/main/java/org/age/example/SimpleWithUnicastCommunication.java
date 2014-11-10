8d160711c9af80e38eba0206a5f760f2b63ba9e3
Łukasz Faber <faber@agh.edu.pl>
2014-11-10 15:58:34 +0100

    Unicast communication example.

---
+++ src/main/java/org/age/example/SimpleWithUnicastCommunication.java
@@ -0,0 +1,72 @@
+/*
+ * Created: 2014-10-07
+ * $Id$
+ */
+
+package org.age.example;
+
+import static com.google.common.base.MoreObjects.toStringHelper;
+
+import org.age.compute.api.MessageListener;
+import org.age.compute.api.UnicastMessenger;
+import org.age.compute.api.WorkerAddress;
+
+import org.checkerframework.checker.nullness.qual.MonotonicNonNull;
+import org.checkerframework.checker.nullness.qual.NonNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Optional;
+import java.util.concurrent.TimeUnit;
+
+import javax.inject.Inject;
+
+/**
+ * The simplest possible computation. Completely detached and having no dependencies and no friends.
+ */
+public final class SimpleWithUnicastCommunication implements Runnable, MessageListener<@NonNull String> {
+
+	private static final Logger log = LoggerFactory.getLogger(SimpleWithUnicastCommunication.class);
+
+	@Inject @MonotonicNonNull private UnicastMessenger messenger;
+
+	@Override public void run() {
+		log.info("This is the simplest possible example of a computation.");
+		log.info("Unicast messenger: {}.", messenger);
+		log.info("My address: {}.", messenger.address());
+
+		messenger.registerListener(this);
+
+		for (int i = 0; i < 100; i++) {
+			log.info("Iteration {}. Sending message.", i);
+
+			// Find any neighbour
+			final Optional<WorkerAddress> target = messenger.neighbours().stream().findAny();
+
+			if (target.isPresent()) {
+				log.info("To: {}.", target.get());
+				messenger.send(target.get(), "Test message from " + messenger.address());
+			} else {
+				log.info("I am alone :(");
+			}
+
+			try {
+				TimeUnit.SECONDS.sleep(1);
+			} catch (final InterruptedException e) {
+				log.debug("Interrupted.", e);
+				Thread.currentThread().interrupt();
+				return;
+			}
+		}
+
+		messenger.removeListener(this);
+	}
+
+	@Override public String toString() {
+		return toStringHelper(this).toString();
+	}
+
+	@Override public void onMessage(@NonNull final String message) {
+		log.info("Message received: {}.", message);
+	}
+}
