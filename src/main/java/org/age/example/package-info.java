3c2b2b50ec99d7b0cfa5abb09b63a2a95bfcb7c6
Łukasz Faber <faber@agh.edu.pl>
2015-01-18 19:56:28 +0100

    Add package-info to examples.

---
+++ src/main/java/org/age/example/package-info.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (C) 2014 Intelligent Information Systems Group.
+ *
+ * This file is part of AgE.
+ *
+ * AgE is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * AgE is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with AgE.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * Created: 04.01.2015.
+ */
+
+/**
+ * Various computational examples.
+ *
+ * This package contains various examples. They usually do nothing, only show how to use particular tools provided by
+ * the platform.
+ */
+package org.age.example;
